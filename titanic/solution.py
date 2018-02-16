f631e26d645bf954a3b5a8f50acd1780d046d172
Alexander Saprykin <alex_afsoc@mail.ru>
2018-02-16 19:48:58 +0300

    Added count_first_class in percentage

--- titanic/solution.py
+++ titanic/solution.py
@@ -7,7 +7,13 @@ with open("count_sex.txt", mode="w") as f:
     f.write(f"{count_sex['male']} {count_sex['female']}")
 
 count_survived = data["Survived"].value_counts()
-per_survived = round(count_survived[1]/(count_survived[0]+count_survived[1]) * 100, 2)
+per_survived = round(count_survived[1]/sum(count_survived) * 100, 2)
 
 with open("count_survived.txt", mode="w") as f:
     f.write(f"{per_survived}")
+
+count_first_class = data["Pclass"].value_counts()
+per_first_class = round(count_first_class[1]/sum(count_first_class) * 100, 2)
+
+with open("count_first_class.txt", mode="w") as f:
+    f.write(f"{per_first_class}")
