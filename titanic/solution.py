1e10fbbf7847da8a8ca4d81c7400db4f5078f854
Alexander Saprykin <alex_afsoc@mail.ru>
2018-02-16 19:54:54 +0300

    Refactor

--- titanic/solution.py
+++ titanic/solution.py
@@ -1,19 +1,24 @@
+import os
+
 import pandas as pd
 
 data = pd.read_csv("titanic.csv", index_col="PassengerId")
 count_sex = data["Sex"].value_counts()
 
-with open("count_sex.txt", mode="w") as f:
+if not os.path.exists("output"):
+    os.mkdir(path="output")
+
+with open(os.path.join("output", "count_sex.txt"), mode="w") as f:
     f.write(f"{count_sex['male']} {count_sex['female']}")
 
 count_survived = data["Survived"].value_counts()
-per_survived = round(count_survived[1]/sum(count_survived) * 100, 2)
+per_survived = round(count_survived[1] / sum(count_survived) * 100, 2)
 
-with open("count_survived.txt", mode="w") as f:
+with open(os.path.join("output", "count_survived.txt"), mode="w") as f:
     f.write(f"{per_survived}")
 
 count_first_class = data["Pclass"].value_counts()
-per_first_class = round(count_first_class[1]/sum(count_first_class) * 100, 2)
+per_first_class = round(count_first_class[1] / sum(count_first_class) * 100, 2)
 
-with open("count_first_class.txt", mode="w") as f:
+with open(os.path.join("output", "count_first_class.txt"), mode="w") as f:
     f.write(f"{per_first_class}")
