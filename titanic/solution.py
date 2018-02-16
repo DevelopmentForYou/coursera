edc43a6de36de3b086fc2aabca3570c7de697def
Alexander Saprykin <alex_afsoc@mail.ru>
2018-02-16 19:29:04 +0300

    Added count_survived in percentage

--- titanic/solution.py
+++ titanic/solution.py
@@ -1,8 +1,13 @@
-import pandas
+import pandas as pd
 
-data = pandas.read_csv("titanic.csv", index_col="PassengerId")
+data = pd.read_csv("titanic.csv", index_col="PassengerId")
 count_sex = data["Sex"].value_counts()
-
 
 with open("count_sex.txt", mode="w") as f:
     f.write(f"{count_sex['male']} {count_sex['female']}")
+
+count_survived = data["Survived"].value_counts()
+per_survived = round(count_survived[1]/(count_survived[0]+count_survived[1]) * 100, 2)
+
+with open("count_survived.txt", mode="w") as f:
+    f.write(f"{per_survived}")
