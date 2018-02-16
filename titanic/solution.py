695688c262fb622815b986424249c3de36ff8374
Alexander Saprykin <alex_afsoc@mail.ru>
2018-02-16 20:04:54 +0300

    Added mean and median for age

--- titanic/solution.py
+++ titanic/solution.py
@@ -22,3 +22,9 @@ per_first_class = round(count_first_class[1] / sum(count_first_class) * 100, 2)
 
 with open(os.path.join("output", "count_first_class.txt"), mode="w") as f:
     f.write(f"{per_first_class}")
+
+age_mean = data["Age"].mean()
+age_median = data["Age"].median()
+
+with open(os.path.join("output", "age_mean.txt"), mode="w") as f:
+    f.write(f"{round(age_mean, 2)} {age_median}")
